5a48591f9205dd2b885a1641919b882bd224a165
Archit Bansal <archit.bansal.93@gmail.com>
2021-07-13 21:46:39 +0200

    Updated scaling plots, opted for log-scale only.

--- lrz_scripts/generate_scaling_plots.py
+++ lrz_scripts/generate_scaling_plots.py
@@ -4,9 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 pth = Path("/home/archit/thesis/scaling_plot_stats")
+
+log_scale_axes = True
+# log_scale_axes = False
+
 budgets = {"1.0", "2.0"}
 # budgets = {"1.0"}
 # budgets = {"2.0"}
+
 nnodes, nconfigs = list(), list()
 for d in pth.iterdir():
     stats = d / "stats.json"
@@ -18,7 +23,7 @@ for d in pth.iterdir():
     nnodes.append(st["nnodes"])
     nconfigs.append(sum([v for b, v in st["nconfigs"].items() if b in budgets]))
 
-x = np.array(nnodes) * 5 # Since each job was run for 5 hours
+x = np.array(nnodes) # Since each job was run for 5 hours
 order = x.argsort()
 x = x[order]
 y = np.array(nconfigs)[order]
@@ -29,19 +34,37 @@ y = np.array(nconfigs)[order]
 #                      "xtick.color": "white", "ytick.color": "white", "grid.color": "silver"})
 
 # Common settings
-plt.rcParams.update({"xtick.labelsize": 12, "ytick.labelsize": 12})
+plt.rcParams.update({"xtick.labelsize": 14, "ytick.labelsize": 14, "axes.labelsize": 16, "axes.titlesize": 16})
 fig, ax = plt.subplots(1, 1, figsize=(16, 9))
 ax: plt.Axes
 
 ax.plot(x, y, marker="*", markerfacecolor="red", markeredgecolor="red", linewidth=2, markersize=12, linestyle="dashed", color="blue")
-ax.set_xlabel("Node hours", size=14)
+
+if log_scale_axes:
+    ymin = np.min(y) / 2
+    ymax = np.max(y) * 2
+else:
+    ymin = np.min(y) * 0.8
+    ymax = np.max(y) * 1.2
+
+ax.set_ylim(ymin, ymax)
+ax.vlines(x, ymin=ymin, ymax=ymax, color="gray", linestyles="--")
+
+for nn in nnodes:
+    ax.text(x=nn, y=ymin, s=nn, fontsize=12)
+
+ax.set_xlabel("# Nodes (5 hours per node)", size=14)
 ax.set_ylabel("# Evaluations", size=14)
 ax.set_title("Scaling plot for throughput against node-hours spent.", size=16)
-ax.set_xscale("log")
-ax.set_yscale("log")
-# ax.set_yscale()
-ax.grid()
+
+if log_scale_axes:
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+
+ax.grid(which="both")
 # plt.show()
 fig.tight_layout()
-fig.savefig("scaling_plot_logscale.pdf")
-# fig.savefig("scaling_plot.pdf")
+if log_scale_axes:
+    fig.savefig("scaling_plot_logscale.pdf")
+else:
+    fig.savefig("scaling_plot.pdf")
