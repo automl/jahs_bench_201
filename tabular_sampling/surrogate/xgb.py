9936af159810855a935ec20d75a8ce2c5d77dccc
Archit Bansal <archit.bansal.93@gmail.com>
2022-01-01 10:59:29 +0100

    Added script for surrogate training.

--- tabular_sampling/surrogate/xgb.py
+++ tabular_sampling/surrogate/xgb.py
@@ -17,8 +17,7 @@ ConfigType = Union[dict, cs.Configuration]
 
 
 class XGBSurrogate:
-    """ A surrogate model based on XGBoost. Adapted from naslib.predictors.tree.xgb.XGBoost at
-    https://github.com/automl/NASLib/blob/6506a700f4d4764f9c58c918efb6769967b55669/naslib/predictors/trees/xgb.py """
+    """ A surrogate model based on XGBoost. """
 
     config_space: cs.ConfigurationSpace
     encoders: dict
@@ -87,80 +86,7 @@ class XGBSurrogate:
         # Both initializes some internal attributes as well as performs a sanity test
         self.set_random_hyperparams()  # Sets default hyperparameters
 
-    # def _encode_single(self, config: ConfigType) -> dict:
-    #     """ Encode a single configuration. """
-    #
-    #     if isinstance(config, cs.Configuration):
-    #         config = config.get_dictionary()
-    #
-    #     if isinstance(config, dict):
-    #         encodings: Sequence[dict] = [self.encoders[p](v) for p, v in config.items()]
-    #         # Concatenate all the encodings into one dict
-    #         config: dict = functools.reduce(lambda c, e: {**c, **e}, encodings)
-    #     else:
-    #         TypeError(f"Unrecognized input type: {type(config)}. Must be {ConfigType}.")
-    #
-    #     return config
-    #
-    # def _encode_series(self, param_values: pd.Series) -> pd.DataFrame:
-    #     """ Encode a Pandas Series that describes multiple values of a single parameter type. The name of the Series
-    #     'param_values' is treated as the parameter name. This can, thus, be directly passed to the method 'apply()' of
-    #     a pandas.DataFrame instance in order to encode the entire DataFrame of complete configurations. Since different
-    #     kinds of encodings can generate either a Series or a DataFrame object (e.g. one-hot encoding), any Series
-    #     encodings are returned as DataFrame objects as well. Such DataFrames have a single column: the name of the
-    #     Series. """
-    #
-    #     encodings = self.encoders[param_values.name](param_values)
-    #     if isinstance(encodings, pd.Series):
-    #         encodings = encodings.to_frame(encodings.name)
-    #
-    #     return encodings
-    #
-    # def encode(self, features: Union[ConfigType, List[ConfigType], pd.DataFrame]) -> \
-    #         Union[dict, List[dict], pd.DataFrame]:
-    #     """ Encode the given features data, assumed to be either a single configuration or a number of configurations,
-    #     according to the defined rules for this model's config space. When passing a DataFrame, care must be taken that
-    #     each row corresponds to a single configuration and each column to a single known parameter name. """
-    #
-    #     if isinstance(features, typingx.get_args(ConfigType)):
-    #         encodings: dict = self._encode_single(features)
-    #         xheaders = list(encodings.keys())
-    #
-    #         # Ensure consistency of ordering
-    #         if self.feature_headers is not None and xheaders is not None and self.feature_headers != xheaders:
-    #             encodings = {h: encodings[h] for h in self.feature_headers}
-    #
-    #     elif isinstance(features, list):
-    #         encodings = list(map(self._encode_single, features))
-    #         xheaders = list(encodings[0].keys())
-    #
-    #         # Ensure consistency of ordering
-    #         if self.feature_headers is not None and xheaders is not None and self.feature_headers != xheaders:
-    #             encodings = map(lambda enc: {h: enc[h] for h in self.feature_headers}, encodings)
-    #
-    #     elif isinstance(features, pd.DataFrame):
-    #         encodings: pd.DataFrame = pd.concat([self._encode_series(features[c]) for c in features.columns], axis=1)
-    #         xheaders = encodings.columns.tolist()
-    #
-    #         # Ensure consistency of ordering
-    #         if self.feature_headers is not None and xheaders is not None and self.feature_headers != xheaders:
-    #             encodings = encodings[self.feature_headers]
-    #     else:
-    #         raise TypeError(f"Unrecognized input type for encode: {type(features)}. Must be "
-    #                         f"{Union[ConfigType, Sequence[ConfigType], pd.DataFrame]}.")
-    #
-    #     if self.feature_headers is None:
-    #         self.feature_headers = xheaders
-    #
-    #     return encodings
-
-    # def get_dataset(self, encodings: pd.DataFrame, labels: pd.DataFrame = None) -> xgb.DMatrix:
-    #     if labels is None:
-    #         return xgb.DMatrix(data=encodings, nthread=-1)
-    #     else:
-    #         return xgb.DMatrix(data=encodings, label=labels.sub(self.ymean, axis=1).div(self.ystd, axis=1), nthread=-1)
-
-    def _get_simple_pipeline(self) -> sklearn.pipeline.Pipeline:
+    def _get_simple_pipeline(self, multiout: bool = True) -> sklearn.pipeline.Pipeline:
         """
         Get a Pipeline instance that can be used to train a new surrogat model. This is the simplest available pipeline
         that simply normalizes all outputs and fits an XGBoost regressor to each regressand to be predicted. HPO is
@@ -179,12 +105,17 @@ class XGBSurrogate:
 
         xgboost_estimator = xgb.sklearn.XGBRegressor(
             n_estimators=500, tree_method="gpu_hist" if self.use_gpu else "auto", **self.hyperparams)
-        multi_regressor = sklearn.multioutput.MultiOutputRegressor(estimator=xgboost_estimator, n_jobs=-1)
-
-        pipeline_steps = [
-            ("preprocess", feature_preprocessing),
-            ("multiout", multi_regressor)
-        ]
+        if multiout:
+            multi_regressor = sklearn.multioutput.MultiOutputRegressor(estimator=xgboost_estimator, n_jobs=-1)
+            pipeline_steps = [
+                ("preprocess", feature_preprocessing),
+                ("multiout", multi_regressor)
+            ]
+        else:
+            pipeline_steps = [
+                ("preprocess", feature_preprocessing),
+                ("estimator", xgboost_estimator)
+            ]
         pipeline = sklearn.pipeline.Pipeline(steps=pipeline_steps)
 
         return pipeline
@@ -222,10 +153,11 @@ class XGBSurrogate:
         #  method to prevent overfitting, analogous to cutting off NN training after a certain number of epochs.
 
         # TODO: Revise scoring and CV split generation
-        pipeline = self._get_simple_pipeline()
+        num_regressands = labels.columns.size
+        pipeline = self._get_simple_pipeline(multiout=num_regressands > 1)
         if perform_hpo:
-            hpo_search_space = {f"multiout__estimator__{k}": v for k, v in self._hpo_search_space.items()}
-            num_regressands = labels.columns.size
+            estimator_prefix = f"{'multiout__' * (num_regressands > 1)}estimator"
+            hpo_search_space = {f"{estimator_prefix}__{k}": v for k, v in self._hpo_search_space.items()}
             # trainer = sklearn.model_selection.HalvingRandomSearchCV(
             #     pipeline, param_distributions=hpo_search_space, resource="multiout__estimator__n_estimators",
             #     random_state=random_state, factor=2, max_resources=self.estimators_per_output * num_regressands,
