cb9c52733695e7335a7cab1982d824c762eee2fa
worstseed <maciejanowski@gmail.com>
2022-06-08 16:53:47 +0200

    Remove src/ from gitignore

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "pandas >=1.3",
         "scipy >=1.7",
         "scikit-learn >=1.0",
-        "xgboost >= 1.5, <1.6",
+        "xgboost >= 1.5",
         "ConfigSpace >= 0.4, <0.5"
     ]
 )
