3383d8669a89e4a3ba240903b247e2788651b92a
Archit Bansal <archit.bansal.93@gmail.com>
2022-06-16 01:35:38 +0200

    Fixed: Porting error for surrogate utils, again.

--- jahs_bench_201/surrogate/utils.py
+++ jahs_bench_201/surrogate/utils.py
@@ -9,7 +9,7 @@ import pandas as pd
 import sklearn.base
 import yacs.config as config
 
-from jahs_bench_201.surrogate import config
+from jahs_bench_201.surrogate import config as surrogate_config
 from jahs_bench_201.surrogate import loss
 
 _log = logging.getLogger(__name__)
@@ -19,9 +19,9 @@ def custom_loss_function(loss_params: config.CfgNode) -> Callable:
     """ Given a configuration, construct a custom loss function. """
 
     func_map = {
-        constants.default_base_loss_configs.exp_lower_bound.name: loss.exponential_bounds,
-        constants.default_base_loss_configs.exp_upper_bound.name: loss.exponential_bounds,
-        constants.default_base_loss_configs.se.name: loss.squared_error
+        surrogate_config.default_base_loss_configs.exp_lower_bound.name: loss.exponential_bounds,
+        surrogate_config.default_base_loss_configs.exp_upper_bound.name: loss.exponential_bounds,
+        surrogate_config.default_base_loss_configs.se.name: loss.squared_error
     }
 
     funcs = []
@@ -40,19 +40,19 @@ def load_pipeline_config(*params) -> config.CfgNode:
     """ Given a list of parameters, interprets the list as (key, value) pairs and
     converts them into an appropriate pipeline configuration. """
 
-    pipeline_config = constants.default_pipeline_config.clone()
+    pipeline_config = surrogate_config.default_pipeline_config.clone()
     pipeline_config.defrost()
     pipeline_config.set_new_allowed(True)
     pipeline_config.merge_from_list(params)
 
     try:
-        loss_type = constants.RegressionLossFuncTypes(pipeline_config.loss)
+        loss_type = surrogate_config.RegressionLossFuncTypes(pipeline_config.loss)
     except ValueError as e:
         raise ValueError(
             f"The loss function for the pipeline should be a right-hand string from the "
-            f"following mapping:\n{list(constants.RegressionLossFuncTypes)}") from e
+            f"following mapping:\n{list(surrogate_config.RegressionLossFuncTypes)}") from e
 
-    if loss_type is constants.RegressionLossFuncTypes.squared_error:
+    if loss_type is surrogate_config.RegressionLossFuncTypes.squared_error:
         return pipeline_config
 
     # Assume it's a custom loss function
@@ -60,11 +60,11 @@ def load_pipeline_config(*params) -> config.CfgNode:
         for f in pipeline_config.loss_params.funcs:
             if not f:
                 continue
-            _ = constants.default_base_loss_configs(f)
+            _ = surrogate_config.default_base_loss_configs(f)
     except ValueError as e:
         raise ValueError(
             f"Unrecognized base loss function {f}, must be either '' or one of "
-            f"{list(constants.default_base_loss_configs.__members__.keys())}") from e
+            f"{list(surrogate_config.default_base_loss_configs.__members__.keys())}") from e
 
     loss_params = pipeline_config.loss_params
     for i, p in enumerate(loss_params.params):
